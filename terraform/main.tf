c26af4dcfa25f4c728ce23316a7a5fbbe1d9ac4b
RobertsKSmits <90193490+Nevelin-W@users.noreply.github.com>
2025-03-05 11:12:01 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,54 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_s3_bucket" "static_website" {
+  bucket = "my-static-website-bucket"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::my-static-website-bucket/*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "index.html"
+  content      = "<html><body><h1>Hello, World!</h1></body></html>"
+  content_type = "text/html"
+  acl          = "public-read"
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
+}
